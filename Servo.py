208d31c58cd735de3eb983f3b307186124bd6e78
Joachim Hummel <joachim.hummel@gmail.com>
2017-03-30 12:10:42 +0200

    Change Steps Arg and add GPIO Cleanup

--- Servo.py
+++ Servo.py
@@ -12,7 +12,7 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
 
-StepPins = [17,22,23,24]
+StepPins = [5,6,13,19]
  
 # Set all pins as output
 for pin in StepPins:
@@ -31,18 +31,19 @@ Seq = [[1,0,0,1],
         
 StepCount = len(Seq)
 StepDir = int(sys.argv[1]) 
-# Set to 1 or 2 for clockwise
-# Set to -1 or -2 for anti-clockwise
- 
-# Read wait time from command line
 WaitTime = int(sys.argv[2])/float(1000)
+Steps = int(sys.argv[3])
+
+# StepDir set to 1 or 2 for clockwise
+# STepDir set  to -1 or -2 for anti-clockwise
+ 
  
 # Initialise variables
 StepCounter = 0
  
 # Start main loop
 # Repeats until amount of steps is reached
-for i in range(int(sys.argv[3]):  
+for i in range(Steps):
  
   for pin in range(0,4):
     xpin=StepPins[pin]# Get GPIO
